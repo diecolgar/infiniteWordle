aea8ae938fa2174500e2b1b86e6e27ca78f57710
diecolgar <diecolgar@gmail.com>
2022-02-13 22:04:28 +0100

    13 February 2022

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function getRandWord() {
     fetch('palabras_todas.txt')
     .then(response => response.text())
     .then((response) => {
-        allWords = response.split('\r\n')
+        allWords = response.split('\n')
             function random_item(item) {
                 return item[Math.floor(Math.random()*item.length)];
             }
